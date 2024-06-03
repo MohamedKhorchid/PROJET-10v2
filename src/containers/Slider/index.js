9469895ffb494508a026fc68eb3f0d976dfe9ea7
mohamed khorchid <mohamed.khorchid.ocs@gmail.com>
2024-06-03 15:29:02 +0200

    corrections

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -25,14 +25,12 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        // ajout de la div manquante qui englobe l'image, les informations et les bullet point
+        <div key={event.title}>
           <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
+            className={`SlideCard SlideCard--${ index === idx ? "display" : "hide" }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img src="{event.cover}" alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
@@ -44,17 +42,18 @@ const Slider = () => {
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {/* Ajout de la fonctionnalité permettant de faire défiler les bullet point du caroussel */}
-              {byDateDesc?.map((_, radioIdx) => (
+              {byDateDesc?.map((dot, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={dot.title}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
